408b459d2d705afa40f58ac468ecc6acc42c80ec
jaekwak02 <jaekwak02@gmail.com>
2018-07-10 21:22:34 -0500

    onVisible trigger

--- src/components/animated-hex-pattern.vue
+++ src/components/animated-hex-pattern.vue
@@ -1,14 +1,16 @@
 <template>
 	<div class="animated-hex-pattern">
-		<svg viewBox="0 0 100 100" class="visual">
-			<g v-for="layer in layers" :key="layer.id">
-				<g v-for="(side, sideIndex) in layer.sides" :key="side.id">
-					<g :transform="`rotate(${sideIndex * 60 + side.rotation} 50 50)`">
-						<polygon :transform="`translate(${polygon.translation})`" :points="polygon.points" v-for="polygon in side.polygons" :key="polygon.id" :fill="polygon.color"></polygon>
+		<transition appear name="instant">
+			<svg viewBox="0 0 100 100" class="visual">
+				<g v-for="layer in layers" :key="layer.id">
+					<g v-for="(side, sideIndex) in layer.sides" :key="side.id">
+						<g :transform="`rotate(${sideIndex * 60 + side.rotation} 50 50)`">
+							<polygon :transform="`translate(${polygon.translation})`" :points="polygon.points" v-for="polygon in side.polygons" :key="polygon.id" :fill="polygon.color"></polygon>
+						</g>
 					</g>
 				</g>
-			</g>
-		</svg>
+			</svg>
+		</transition>
 	</div>
 </template>
 
@@ -33,6 +35,7 @@ export default {
 	name: 'falling-bricks',
 	data: function() {
 		return {
+			animated: false,
 			layers: []
 		}
 	},
@@ -59,82 +62,96 @@ export default {
 					}
 				})
 				.start();
+		},
+		Animate: function() {
+			this.CreateLayers(4);
+
+			for (var i = 0; i < 6; i ++) {
+				this.layers[0].sides.push(CreateCenter(60));
+				this.layers[1].sides.push(CreateLayer2(180));
+				this.layers[2].sides.push(CreateLayer3(60));
+				this.layers[3].sides.push(CreateLayer4(60));
+			}
+
+			var t = this;
+			
+			var centerTranslation = {
+				translation: 20
+			}
+			new TWEEN.Tween(centerTranslation)
+				.to({ translation: 0 }, 2000)
+				.easing(TWEEN.Easing.Quadratic.Out)
+				.onUpdate(function() {
+					for (var i = 0; i < 6; i++) {
+						t.layers[0].sides[i].polygons.forEach(p => p.translation = centerTranslation.translation);
+					}
+				})
+				.delay(1000)
+				.start();
+
+			var centerRotation = { rotation: 60 }
+			new TWEEN.Tween(centerRotation)
+				.to({ rotation: 0 }, 1000)
+				.easing(TWEEN.Easing.Quadratic.Out)
+				.onUpdate(function() {
+					t.layers[0].sides.forEach(s => s.rotation = centerRotation.rotation)
+				})
+				.delay(1000)
+				.start();
+
+			var layer2translation = { translation: 40 };
+			new TWEEN.Tween(layer2translation)
+				.to({ translation: 0 }, 1000)
+				.easing(TWEEN.Easing.Quadratic.Out)
+				.onUpdate(function() {
+					t.layers[1].sides.forEach(s => s.polygons.forEach(p => p.translation = layer2translation.translation));
+				})
+				.start();
+
+			var layer2rotation = { rotation: 180 };
+			new TWEEN.Tween(layer2rotation)
+				.to({ rotation: 0 }, 3000)
+				.easing(TWEEN.Easing.Quadratic.Out)
+				.onUpdate(function() {
+					t.layers[1].sides.forEach(s => s.rotation = layer2rotation.rotation)
+				})
+				.start();
+
+			var layer3translation = { translation: 30 };
+			new TWEEN.Tween(layer3translation)
+				.to({ translation: 10 }, 1000)
+				.easing(TWEEN.Easing.Quadratic.Out)
+				.onUpdate(function() {
+					t.layers[2].sides.forEach(s => s.polygons.forEach(p => p.translation = layer3translation.translation));
+				})
+				.start();
+
+			var layer4translation = { translation: 10 };
+			new TWEEN.Tween(layer4translation)
+				.to({ translation: 30 }, 1500)
+				.easing(TWEEN.Easing.Quadratic.Out)
+				.onUpdate(function() {
+					t.layers[3].sides.forEach(s => s.polygons.forEach(p => p.translation = layer4translation.translation));
+				})
+				.start();
 		}
 	},
 	created: function() {
 
+		if (document.hidden) {
+			document.addEventListener("visibilitychange", () => {				
+				if (!document.hidden && !this.animated)  {
+					this.animated = true;
+					this.Animate();
+				}
+			});
+		} else {
+			this.Animate();
+		}
+
 		EventBus.$on('animate-hex', this.AnimateHex);
 
-		this.CreateLayers(4);
-
-		for (var i = 0; i < 6; i ++) {
-			this.layers[0].sides.push(CreateCenter(60));
-			this.layers[1].sides.push(CreateLayer2(180));
-			this.layers[2].sides.push(CreateLayer3(60));
-			this.layers[3].sides.push(CreateLayer4(60));
-		}
-
-		var t = this;
 		
-		var centerTranslation = {
-			translation: 20
-		}
-		new TWEEN.Tween(centerTranslation)
-			.to({ translation: 0 }, 2000)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(function() {
-				for (var i = 0; i < 6; i++) {
-					t.layers[0].sides[i].polygons.forEach(p => p.translation = centerTranslation.translation);
-				}
-			})
-			.delay(1000)
-			.start();
-
-		var centerRotation = { rotation: 60 }
-		new TWEEN.Tween(centerRotation)
-			.to({ rotation: 0 }, 1000)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(function() {
-				t.layers[0].sides.forEach(s => s.rotation = centerRotation.rotation)
-			})
-			.delay(1000)
-			.start();
-
-		var layer2translation = { translation: 40 };
-		new TWEEN.Tween(layer2translation)
-			.to({ translation: 0 }, 1000)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(function() {
-				t.layers[1].sides.forEach(s => s.polygons.forEach(p => p.translation = layer2translation.translation));
-			})
-			.start();
-
-		var layer2rotation = { rotation: 180 };
-		new TWEEN.Tween(layer2rotation)
-			.to({ rotation: 0 }, 3000)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(function() {
-				t.layers[1].sides.forEach(s => s.rotation = layer2rotation.rotation)
-			})
-			.start();
-
-		var layer3translation = { translation: 30 };
-		new TWEEN.Tween(layer3translation)
-			.to({ translation: 10 }, 1000)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(function() {
-				t.layers[2].sides.forEach(s => s.polygons.forEach(p => p.translation = layer3translation.translation));
-			})
-			.start();
-
-		var layer4translation = { translation: 10 };
-		new TWEEN.Tween(layer4translation)
-			.to({ translation: 30 }, 1500)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(function() {
-				t.layers[3].sides.forEach(s => s.polygons.forEach(p => p.translation = layer4translation.translation));
-			})
-			.start();
 	}
 }
 
@@ -271,5 +288,15 @@ function GenerateColor() {
 		width: 100%;
 		height: 100%;
 	}
+}
+
+.instant-enter {
+	opacity: 0;
+}
+.instant-enter-to {
+	opacity: 1;
+}
+.instant-enter-active {
+	transition: all .5s;
 }
 </style>
